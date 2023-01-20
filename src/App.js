fcc43547f126d0b035bf6f5a827713fb0ac76988
Faizan <faiz63440@gmail.com>
2023-01-20 19:50:29 +0530

    reacr2

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Forms from './Components/Forms';
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1>EMPLOYEE FEEDBACK FORM</h1>
+     <Forms ></Forms>
+    </div>
+  );
+}
+
+export default App;
